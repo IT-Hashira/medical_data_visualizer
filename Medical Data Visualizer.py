0849aa224c99767c2e15b82063b0b441603d3e33
Constantine <156456160+IT-Hashira@users.noreply.github.com>
2024-05-14 16:29:19 +0300

    Medical Data Visualizer.py

--- Medical Data Visualizer.py
+++ Medical Data Visualizer.py
@@ -13,7 +13,7 @@ import numpy as np
 # In[73]:
 
 
-# 1
+
 df = pd.read_csv("medical_examination.csv")
 df.head()
 
@@ -21,22 +21,22 @@ df.head()
 # In[74]:
 
 
-# 2 (Insertion of ''overweight'' column and BMI calculation, 0 = good <25 BMI, 1= bad >25 BMI)
+#Insertion of ''overweight'' column and BMI calculation, 0 = good <25 BMI, 1= bad >25 BMI
 df.insert(7 , 'overweight', True)
 df['overweight'] = 0
 for index, row in df.iterrows():
     if (row['weight']/(row['height']/100)**2) > 25:
         df.at[index, "overweight"] = 1
 
-# 3 Data normalization 0 = good, 1 = bad
-# 4Normalization of 'cholesterol' column
+# Data normalization 0 = good, 1 = bad
+# Normalization of 'cholesterol' column
 for index, row in df.iterrows():
     if row['cholesterol'] <= 1:
         df.at[index, 'cholesterol'] = 0
     else:
         df.at[index, 'cholesterol'] = 1
         
-# 4 Normalization of 'gluc' column       
+# Normalization of 'gluc' column       
 for index, row in df.iterrows():
     if row['gluc'] <= 1:
         df.at[index, 'gluc'] = 0
@@ -47,23 +47,17 @@ for index, row in df.iterrows():
 
 def draw_cat_plot():
     
-    # 5 
+    #categorical values filtering
     categorical_cols = ['sex', 'cholesterol', 'gluc', 'smoke', 'alco', 'active']
     df_long_data = pd.melt(df, id_vars=['id', 'cardio'], value_vars=categorical_cols, var_name='variable', value_name='value')
 
-    # 6
     df_cat = df_long_data.groupby(['cardio', 'variable', 'value'], as_index=False).size()
     
-
-    # 7
+    #plotting based on categorical values
     g = sns.catplot(x='variable', y='size', hue='value', col='cardio', data=df_cat, kind='bar')
     
-    
-    # 8
     fig = g.fig
 
-
-    # 9
     fig.savefig('catplot.png')
     return fig
 
@@ -73,9 +67,9 @@ def draw_cat_plot():
 # In[76]:
 
 
-# 10
+
 def draw_heat_map(df):
-    # Clean the data
+    # data cleaning
     df_cleaned = df[
         (df['ap_lo'] <= df['ap_hi']) &
         (df['height'] >= df['height'].quantile(0.025)) &
@@ -84,28 +78,20 @@ def draw_heat_map(df):
         (df['weight'] <= df['weight'].quantile(0.975))
     ]
 
-    # Create a correlation matrix
+    # correlation matrix
     corr_matrix = df_cleaned.corr()
 
-    # Plot the correlation matrix
+    # plotting the correlation matrix
     plt.figure(figsize=(10, 8))
     mask = np.triu(corr_matrix)  # Keep only the lower triangle of the correlation matrix
     sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', mask=mask, fmt='.2f', vmin=0.0, vmax=0.3, square=True)
     plt.title('Correlation Matrix')
     plt.savefig('heatmap.png')
     plt.show()
-    return
-plt.show(draw_cat_plot())
-plt.show(draw_heat_map(df))
+    return fig
 
 
-# In[77]:
 
-
-df.head()
-
-
-# In[ ]:
 
 
 
